a93fbbe10d17d777ca436985c60b429ea8979640
SamThomas <Sam.Thomas@ca.com>
2018-02-09 01:37:53 -0800

    Some Edits

---
+++ src/02_package/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"02_package/stringutil"
+	"02_package/icomefromalaska"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
